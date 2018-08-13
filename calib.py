f51afe06fcfc2019112960ff931979a3c2204daa
jessie <610891637@qq.com>
2018-08-13 21:27:38 +0800

    calibration simplification

--- calib.py
+++ calib.py
@@ -4,47 +4,52 @@ import glob
 
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
-
-# Arrays to store object points and image points from all the images.
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
-
 images = glob.glob('left/*.jpg')
-print(images.__len__())
-for fname in images:
-    img = cv2.imread(fname)
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-    # Find the chxess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
+def find_points():
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((6 * 7, 3), np.float32)
+    objp[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)  # row by row, left to right in every row, z value defines 0
 
-    # If found, add object points, image points (after refining them)
-    if ret == True:
-        objpoints.append(objp)
+    # Arrays to store object points and image points from all the images.
+    objpoints = []  # 3d point in real world space
+    imgpoints = []  # 2d points in image plane.
 
-        corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
-        imgpoints.append(corners2)
+    for fname in images:
+        img = cv2.imread(fname)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        # Draw and display the corners
-        img = cv2.drawChessboardCorners(img, (7,6), corners2,ret)
-        cv2.imshow('img',img)
-        cv2.waitKey(1)
+        # Find the chess board corners
+        ret, corners = cv2.findChessboardCorners(gray, (7, 6))
 
-cv2.destroyAllWindows()
+        # If found, add object points, image points (after refining them)
+        if ret == True:
+            objpoints.append(objp)
 
-ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-print(mtx)
-img = cv2.imread('left/left12.jpg')
-h,  w = img.shape[:2]
-newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
-# undistort
-dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+            corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+            imgpoints.append(corners2)  # add the corner points corresponding to the 3D points
 
-# crop the image
-x,y,w,h = roi
-dst = dst[y:y+h, x:x+w]
-cv2.imwrite('calibresult.png',dst)
+    imageSize = gray.shape[::-1]
+    return objpoints, imgpoints, imageSize
+
+def calibration():
+    objpoints, imgpoints, imageSize = find_points()
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, imageSize, None, None)
+    return ret, mtx, dist, rvecs, tvecs
+
+
+def undistortion(imgname, outputname):
+    ret, mtx, dist, rvecs, tvecs = calibration()
+    img = cv2.imread(imgname)
+    h, w = img.shape[:2]
+    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+    # undistort
+    dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+
+    # crop and save the image
+    x, y, w, h = roi
+    dst = dst[y:y + h, x:x + w]
+    cv2.imwrite(outputname, dst)
+
+if __name__ == "__main__":
+    undistortion("left/left01.jpg", "calib_result01.png")
