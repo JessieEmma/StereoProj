a8a7c4b029f8780adcfe80b0e4f4ba8618b746bf
jessie <610891637@qq.com>
2018-08-23 16:15:23 +0800

    stereo matching

--- StereoMatching/disparity.py
+++ StereoMatching/disparity.py
@@ -2,11 +2,24 @@ import cv2
 import numpy as np
 
 def get_img(left_img_path, right_img_path):
+    """
+    Get the image of the left and right camera
+    :param left_img_path:
+    :param right_img_path:
+    :return:
+    """
     left_img = cv2.imread(left_img_path)
     right_img = cv2.imread(right_img_path)
     return left_img, right_img
 
 def createSGBM(window_size=3, min_disp=16, max_disp=112):
+    """
+    :param window_size:
+    :param min_disp: the minimun value of disparity
+    :param max_disp: the maximun values of disparity
+    :return:
+    """
+
     num_disp = max_disp - min_disp
 
     stereo = cv2.StereoSGBM_create(minDisparity=min_disp,
@@ -24,6 +37,16 @@ def createSGBM(window_size=3, min_disp=16, max_disp=112):
 
 
 def get_disparity(left_img, right_img, stereo, min_disp=16, max_disp=112, is_show=True):
+    """
+    Get disparity of the two images
+    :param left_img:
+    :param right_img:
+    :param stereo:
+    :param min_disp:
+    :param max_disp:
+    :param is_show:
+    :return:
+    """
     disp = stereo.compute(left_img, right_img).astype(np.float32) / 16.0
     num_disp = max_disp - min_disp
     if is_show:
@@ -40,3 +63,4 @@ if __name__ == "__main__":
     left_img, right_img = get_img("../left/left01.jpg", "../right/right01.jpg")
     disp = get_disparity(left_img, right_img, stereo)
     print("disparity")
+    print(disp)
